61d147cfbd55b119f60977a59d931abb9877d791
DeadMoon0 <94380754+DeadMoon0@users.noreply.github.com>
2026-04-16 23:19:57 +0200

    copy TestFramework.Simple into Repo and connect with Nuget Package

---
+++ TestFramework.Simple/ActionTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TestFramework.Core.Artifacts;
+using TestFramework.Core.Logging;
+using TestFramework.Core.Steps;
+using TestFramework.Core.Steps.Options;
+using TestFramework.Core.Variables;
+
+namespace TestFrameworkSimple;
+
+public class ActionTrigger(Action<IServiceProvider, ScopedLogger, Dictionary<VariableIdentifier, object?>, Dictionary<ArtifactIdentifier, ArtifactInstanceGeneric>> action, VariableReferenceGeneric[] variables, ArtifactIdentifier[] artifacts) : Step<object?>
+{
+    public override string Name => "Action Trigger";
+
+    public override string Description => "A Trigger which executes an C# Action";
+
+    public override bool DoesReturn => false;
+
+    public override Step<object?> Clone()
+    {
+        return new ActionTrigger(action, variables, artifacts).WithClonedOptions(this);
+    }
+
+    public override Task<object?> Execute(IServiceProvider serviceProvider, VariableStore variableStore, ArtifactStore artifactStore, ScopedLogger logger, CancellationToken cancellationToken)
+    {
+        action
+        (
+            serviceProvider,
+            logger,
+            variables.ToDictionary(x => x.Identifier ?? throw new ArgumentNullException("identifier", "A variable passed to Action requires a non-null identifier."), x => x.GetValueGeneric(variableStore)),
+            artifacts.ToDictionary(x => x, artifactStore.GetArtifact)
+        );
+        return Task.FromResult((object?)null);
+    }
+
+    public override StepInstance<Step<object?>, object?> GetInstance() => new StepInstance<Step<object?>, object?>(this);
+
+    public override void DeclareIO(StepIOContract contract)
+    {
+        foreach (var item in variables)
+            if (item.HasIdentifier)
+                contract.Inputs.Add(new StepIOEntry(item.Identifier!.Identifier, StepIOKind.Variable));
+        foreach (var item in artifacts)
+            contract.Inputs.Add(new StepIOEntry(item.Identifier, StepIOKind.Artifact));
+    }
+}
---
+++ TestFramework.Simple/Simple.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using TestFramework.Core.Artifacts;
+using TestFramework.Core.Logging;
+using TestFramework.Core.Variables;
+
+namespace TestFrameworkSimple;
+
+public static class Simple
+{
+    public static SimpleTrigger Trigger { get; } = new SimpleTrigger();
+}
+
+public class SimpleTrigger
+{
+    public ActionTrigger Action(Action action)
+    {
+        return new ActionTrigger((_, _, _, _) => action(), [], []);
+    }
+
+    public ActionTrigger Action(Action<Dictionary<VariableIdentifier, object?>> action, params VariableReferenceGeneric[] variables)
+    {
+        return new ActionTrigger((_, _, vars, _) => action(vars), variables, []);
+    }
+
+    public ActionTrigger Action(Action<Dictionary<VariableIdentifier, object?>, Dictionary<ArtifactIdentifier, ArtifactInstanceGeneric>> action, VariableReferenceGeneric[] variables, params ArtifactIdentifier[] artifacts)
+    {
+        return new ActionTrigger((_, _, vars, artifacts) => action(vars, artifacts), variables, artifacts);
+    }
+
+    public ActionTrigger Action(Action<IServiceProvider, ScopedLogger, Dictionary<VariableIdentifier, object?>, Dictionary<ArtifactIdentifier, ArtifactInstanceGeneric>> action, VariableReferenceGeneric[] variables, params ArtifactIdentifier[] artifacts)
+    {
+        return new ActionTrigger(action, variables, artifacts);
+    }
+
+    public MessageBoxTrigger MessageBox(VariableReference<string> msg, VariableReference<string> caption)
+    {
+        return new MessageBoxTrigger(msg, caption);
+    }
+
+    public MessageBoxTrigger MessageBox(VariableReference<string> msg)
+    {
+        return MessageBox(msg, "MessageBox");
+    }
+}
